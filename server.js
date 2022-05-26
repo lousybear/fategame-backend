a33caa0a80bdfed5cada39a4b77cbea42f6203fe
lousybear <ashishkrse@gmail.com>
2022-05-26 19:12:29 +0530

    Port updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const coin = require("./functionsCoin");
 const socket = require("./index");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
