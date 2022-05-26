3a1f32d65bc7fae6a496084bca49a2dc1bf3cda9
lousybear <ashishkrse@gmail.com>
2022-05-26 21:11:31 +0530

    cors enabled

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
+const cors = require("cors");
 app.use(bodyParser.json());
-
+app.use(cors());
 coin.startGame();
 app.use(bodyParser.urlencoded({ extended: false }));
 
